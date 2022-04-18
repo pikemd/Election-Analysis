a5aecedd8a345a64ac13eda15dc464df933d4c2b
pikemd <101365965+pikemd@users.noreply.github.com>
2022-04-17 20:10:54 -0500

    adding files

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+from os import name
+import string
+from tkinter.tix import INTEGER
+from xmlrpc.client import boolean
+
+name = "Molly"
+Country = "united states"
+age = 26
+hourly_wage = 25.99
+satisfied = False
+daily_wage = (hourly_wage*8)
+
+print (5 + 2 * 3)
+print (8 // 5 - 3)
+print (8 + 22 * 2 - 4)
+16 - 3 / 2 + 7 - 1
+3 ** 3 % 5
+5 + 9 * 3 / 2 - 4
